44ec04d3ec7d213cb6b74196a433282767d9b6c2
Jacob Feng <peanutChowder@gmail.com>
2024-01-23 11:16:01 -0700

    added

---
+++ decoding.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct HuffmanNode;
+
+
+
+vector<int> decodeString(const string encodedStr, HuffmanNode *root) {
+    vector<int> decoded;
+}
