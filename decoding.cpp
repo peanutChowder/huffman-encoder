3641f000522694716ae56d25a141381c76bde455
Jacob Feng <peanutChowder@gmail.com>
2024-01-23 11:24:20 -0700

    Finished decoding function + header

--- decoding.cpp
+++ decoding.cpp
@@ -1,12 +1,28 @@
 #include <vector>
 #include <string>
 
+#include "huffmanNode.h"
+
 using namespace std;
-
-struct HuffmanNode;
-
-
 
 vector<int> decodeString(const string encodedStr, HuffmanNode *root) {
     vector<int> decoded;
+    const HuffmanNode *currNode = root;
+
+    for (char bit: encodedStr) {
+        if (bit == '0') {
+            currNode = currNode->left;
+        } else if (bit == '1') {
+            currNode = currNode->right;
+        } else {
+            perror("Invalid bit in decoding!! Aborting");
+            exit(1);
+        }
+
+        if (currNode-> left == nullptr && currNode->right == nullptr) {
+            decoded.push_back(currNode->val);
+            currNode = root;
+        }
+    }
+    return decoded;
 }
---
+++ decoding.h
@@ -0,0 +1,8 @@
+#include <vector>
+#include <string>
+
+#include "huffmanNode.h"
+
+using namespace std;
+
+vector<int> decodeString(const string encodedStr, HuffmanNode *root);
