20b550d7684b5d2f963a860c70c04acaed36bade
Jacob Feng <peanutChowder@gmail.com>
2024-01-23 00:55:36 -0700

    Added tree builder

--- encoding.cpp
+++ encoding.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
 
@@ -11,6 +12,12 @@ struct HuffmanNode {
     HuffmanNode *right;
 
     HuffmanNode(int val, int frequency): val(val), frequency(frequency), left(nullptr), right(nullptr) {}
+};
+
+struct CompareNodes {
+    bool operator() (HuffmanNode *a, HuffmanNode *b) {
+        return a->frequency > b->frequency;
+    }
 };
 
 void getSortedFrequency(vector<pair<int, int>> &repeatFrequency, int * array, int arrayLen) {
@@ -25,4 +32,28 @@ void getSortedFrequency(vector<pair<int, int>> &repeatFrequency, int * array, in
     sort(repeatFrequency.begin(), repeatFrequency.end(), [](const auto &num1, const auto &num2) {
         return num1.second < num2.second;
     });
+}
+
+HuffmanNode* buildTree(vector<pair<int,int>> repeatFrequency) {
+    priority_queue<HuffmanNode*, vector<HuffmanNode*>, CompareNodes> minHeap;
+
+    for (const pair item: repeatFrequency) {
+        minHeap.push(new HuffmanNode(item.first, item.second));
+    }
+
+    while (minHeap.size() > 1) {
+        HuffmanNode *left = minHeap.top();
+        minHeap.pop();
+
+        HuffmanNode *right = minHeap.top();
+        minHeap.pop();
+
+        HuffmanNode *currNode = new HuffmanNode(-1, left->frequency + right->frequency);
+        currNode->left = left;
+        currNode->right = right;
+
+        minHeap.push(currNode);
+    }
+
+    return minHeap.top();
 }
